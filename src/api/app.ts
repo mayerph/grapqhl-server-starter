abe4193f1ed00ef2572425d5442ae020ef7ae38c
mayerph <m.philipp.m@googlemail.com>
2018-12-29 17:47:58 +0100

    code cleanup

--- src/api/app.ts
+++ src/api/app.ts
@@ -43,10 +43,7 @@ const apollo = new ApolloServer({
     subscriptions: {
         onConnect: async (connectionParams: any, webSocket, context) => {
             if (connectionParams.authentication) {
-                //if (true) {
                 const userToken = connectionParams.authToken
-                //const userToken =
-                //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmZkNjQxYTU1YzdkYzFmY2ZjNzA5ZTMiLCJ1c2VybmFtZSI6ImFkbWlubiIsInBhc3N3b3JkIjoiJDJiJDEwJC9GZk9sRHUvaEpvVHV3a0hubzUwa09DVmhSUWtIOEdCTGFZUnJaOEVoLmliZzFPUHVzWnN5IiwiZW1haWwiOiJhZG1pbkBobS5lZHUiLCJyb2xlIjoiNWJmZDIxYzQ4ZTJiMzU1MzY2NzNiMTM1IiwiX192IjowLCJpbWciOiI1YzBlYzZhOTRhMTM4M2Q3YzI1NzA1NjAiLCJpYXQiOjE1NDQ1NTIwNDMsImV4cCI6MTU0NDU1Mzg0M30.w20IityB7v00JMDm-Q1rBKL5XEefPOfHIBvEF3A3Lh4'
                 return createContext(userToken)
             } else {
                 throw new Error('Missing auth token!')
