58d26b563fd8524a39b429c35dbcb0eb2c7e029e
mayerph <m.philipp.m@googlemail.com>
2019-01-03 11:38:51 +0100

    changed permissions for user queries

--- src/api/user/schema/user.query.ts
+++ src/api/user/schema/user.query.ts
@@ -2,8 +2,8 @@ import { gql } from 'apollo-server-express'
 
 const userQuery = gql`
     extend type Query {
-        users: [User!] @hasPermission(requiredPermission: adminUser)
-        user(id: ID!): User @hasPermission(requiredPermission: adminUser)
+        users: [User!] @hasPermission(requiredPermission: adminDefault)
+        user(id: ID!): User @hasPermission(requiredPermission: adminDefault)
         me: User @hasPermission(requiredPermission: readDefault)
     }
 `
